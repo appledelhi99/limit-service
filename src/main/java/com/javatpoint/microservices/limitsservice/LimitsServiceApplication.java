28b9c82e8dbee4c7eaca5bfa4e0213156f5f6e11
abc <abc@example.com>
2022-03-23 15:09:24 +0530

    limit service

---
+++ src/main/java/com/javatpoint/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javatpoint.microservices.limitsservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class LimitsServiceApplication 
+{
+	String loc = "C:\\Users\\shivu\\git-localconfig-repo";
+	
+public static void main(String[] args) 
+{
+SpringApplication.run(LimitsServiceApplication.class, args);
+}
+}
